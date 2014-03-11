12df9078d5e0e84c6c943e6c7d853c85638084fb
Mike <Mike@Mikes-MacBook-Pro.local>
2014-03-12 08:13:24 +1100

    added a test class

---
+++ GIT Test/MKTest.h
@@ -0,0 +1,13 @@
+//
+//  MKTest.h
+//  GIT Test
+//
+//  Created by Mike on 12/03/2014.
+//  Copyright (c) 2014 MKCon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MKTest : NSObject
+
+@end
---
+++ GIT Test/MKTest.m
@@ -0,0 +1,13 @@
+//
+//  MKTest.m
+//  GIT Test
+//
+//  Created by Mike on 12/03/2014.
+//  Copyright (c) 2014 MKCon. All rights reserved.
+//
+
+#import "MKTest.h"
+
+@implementation MKTest
+
+@end
